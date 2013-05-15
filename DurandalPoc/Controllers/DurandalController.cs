1cddbbe18a51ff6e044c9e5bc41d95d55cbef701
Andres Moschini <andresmoschini@gmail.com>
2013-05-15 09:16:48 -0300

    Install Durandal.StarterKit nuget package

---
+++ DurandalPoc/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace DurandalPoc.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
