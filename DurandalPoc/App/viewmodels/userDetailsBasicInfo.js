4de61568480e506c690bc632b25803b9497ff142
Andres Moschini <andresmoschini@gmail.com>
2013-05-15 16:38:38 -0300

    Testing an approach for partials

---
+++ DurandalPoc/App/viewmodels/userDetailsBasicInfo.js
@@ -0,0 +1,11 @@
+﻿define(['durandal/plugins/router', 'durandal/system', 'durandal/app', 'services/repository', 'viewmodels/userDetails'], function (router, system, app, repository, userDetails) {
+    alert("userDetailsBasicInfo");
+    system.log("userDetailsBasicInfo");
+    system.log(userDetails.user());
+
+    var vm = {
+        basicInfo: userDetails.user().BasicInfo
+    };
+
+    return vm;
+});
